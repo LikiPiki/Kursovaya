828ded077bf3defcab85a479f147e274f252fb42
Sergey <sergey.batalev@yandex.ru>
2019-04-15 23:13:17 +0300

    directory for json files

--- main.cpp
+++ main.cpp
@@ -1,11 +1,40 @@
 #include <iostream>
 
 #include "OfficeComputer.h"
+#include "GamingComputer.h"
+#include "StudentComputer.h"
+
+void showMenu();
+void printLine(string line);
 
 int main() {
 
-    OfficeComputer *comp = new OfficeComputer();
-    comp->CreateComputer().writeToFile("office.json");
+    auto *officeComputer = new OfficeComputer();
+    auto *gamingComputer = new GamingComputer();
+    auto *studentComputer = new StudentComputer();
+
+    bool running = true;
+    int mode;
+    while (running) {
+        cout << "Выберите пункт меню:" << endl;
+        showMenu();
+        cin >> mode;
+        switch (mode) {
+            case 0:
+                running = false;
+        }
+    }
 
     return 0;
+}
+
+void showMenu() {
+    printLine("0 - Выход");
+    printLine("1 - Добавить оффисный компьютер");
+    printLine("2 - Добавить игровой компьютер");
+    printLine("3 - Добавить компьютер студента");
+}
+
+void printLine(string line) {
+    cout << line << endl;
 }
